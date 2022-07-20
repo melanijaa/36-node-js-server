bdb9e949690ae7516e8645b79a121072520fb0bc
Melanija <melanija.sibakovska1@gmail.com>
2022-07-20 11:58:51 +0300

    initial

---
+++ index.js
@@ -0,0 +1,22 @@
+import { server } from "./lib/server.js";
+
+const app = {};
+
+app.init = () => {
+    console.log('server bla bla');
+    //sukurti pradinius folderius
+    //sukurti pradinius files
+
+    //prisijungti prie DB;
+    //paleisti (musu) serveri;
+    server.init();
+    
+    //pasikrtojantys procesai:
+    // - istrinti nenaudojamus failus
+    // - su'zip'inti sena informacija
+    // - atsinaujinti API informacija
+}
+
+app.init();
+
+export { app }
